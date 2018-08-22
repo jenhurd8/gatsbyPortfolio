f90c95323203dbab4d26729410a307e496bbbd80
Jennifer Hurd <jenhurd8@gmail.com>
2018-08-21 22:15:46 -0500

    gatsby portfolio started

---
+++ src/components/Header.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Header = props => (
+  <header id="header" className="alt">
+    <span className="logo">
+      <div className="header__main">
+        <div>
+          <h1>Jennifer Hurd</h1>
+
+          <img
+            src={
+              'http://res.cloudinary.com/jjenjjenjjen/image/upload/c_scale,w_183/v1534899352/IMG_20180712_172449701_1_u9rnja.jpg'
+            }
+            alt="Jen"
+            style={{ height: '30%', width: '60%', borderRadius: '50%' }}
+          />
+          <p>Full Stack Developer from the suburbs of Chicago</p>
+        </div>
+        <div>
+          <img
+            src={
+              'http://res.cloudinary.com/jjenjjenjjen/image/upload/v1534887981/chicago-illinois-skyline-skyscrapers-161963_isugq0.jpg'
+            }
+            alt="Chicago"
+            style={{ borderRadius: '3%' }}
+          />
+        </div>
+      </div>
+      <div />
+    </span>
+  </header>
+)
+
+export default Header
---
+++ src/pages/index.js
@@ -0,0 +1,247 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import get from 'lodash/get'
+import Helmet from 'react-helmet'
+import Waypoint from 'react-waypoint'
+
+import Header from '../components/Header'
+import Nav from '../components/Nav'
+import pic01 from '../assets/images/pic01.jpg'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faStroopwafel)
+
+class Index extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      stickyNav: false,
+    }
+  }
+
+  _handleWaypointEnter = () => {
+    this.setState(() => ({ stickyNav: false }))
+  }
+
+  _handleWaypointLeave = () => {
+    this.setState(() => ({ stickyNav: true }))
+  }
+
+  render() {
+    return (
+      <div>
+        <Helmet title={get(this, 'props.data.site.siteMetadata.title')} />
+
+        <Header />
+
+        <Waypoint
+          onEnter={this._handleWaypointEnter}
+          onLeave={this._handleWaypointLeave}
+        />
+        <Nav sticky={this.state.stickyNav} />
+
+        <div id="main">
+          <section id="intro" className="main">
+            <div className="spotlight">
+              <div className="content">
+                <header className="major">
+                  <h2>About Me</h2>
+                </header>
+                <p>
+                  Hi! I'm <b>Jennifer Hurd</b>, a Full Stack Developer with a
+                  strong interest in both technology and helping people. I
+                  really enjoy figuring out how the devices we use everyday work
+                  in greater detail and had put these interests to use in my
+                  previous roles in technical support.
+                </p>
+                <p>
+                  More recently, I found an interest in coding. Technical
+                  support agents resolve a lot of problems, but many of the
+                  issues not resolved are often escalated to software engineers.
+                  I attended a few coding meetups near my house and realized I
+                  could be one of those providing solutions as well as bringing
+                  my own ideas to life! Once I got started I knew this is what I
+                  was meant to do and have been coding ever since.
+                </p>
+                <p>
+                  When I am not coding, I love to spend my time traveling. If
+                  I'm not checking out a cave in Kentucky or combing a beach in
+                  the Bahamas, you can find me thinking of where to go next...
+                </p>
+              </div>
+              <span className="image">
+                <img
+                  src={
+                    'http://res.cloudinary.com/jjenjjenjjen/image/upload/v1531494432/Jen_o9ev3x.jpg'
+                  }
+                  alt="Jen"
+                />
+              </span>
+            </div>
+          </section>
+
+          <section id="first" className="main special">
+            <header className="major">
+              <h2>Skills</h2>
+            </header>
+            <ul className="features">
+              <li>
+                <span className="icon style1 fa fa-github fa-5x" />
+                <h3>Git and Github</h3>
+              </li>
+              <li>
+                <img
+                  src="http://res.cloudinary.com/jjenjjenjjen/image/upload/c_scale,w_100/v1496579062/JavaScript_ciochs.png"
+                  alt="JavaScript"
+                />
+                <h3>JavaScript</h3>
+              </li>
+              <li>
+                <span className="icon style3 fa fa-css3 fa-5x" />
+                <h3>CSS</h3>
+              </li>
+              <li>
+                <img
+                  src="http://res.cloudinary.com/jjenjjenjjen/image/upload/c_scale,w_150/v1534904925/maxresdefault_jjetpo.jpg"
+                  alt="Node"
+                />
+                <h3>Node</h3>
+                <i class="fab fa-react" />
+              </li>
+              <li>
+                <span className="icon style2 fa fa-html5 fa-5x" />
+                <h3>HTML5</h3>
+              </li>
+              <li>
+                <img
+                  src="http://res.cloudinary.com/jjenjjenjjen/image/upload/c_scale,w_100/v1534904093/react-512_ek1bfd.png"
+                  alt="React"
+                />
+                <h3>React</h3>
+              </li>
+              <li>
+                <img
+                  src="http://res.cloudinary.com/jjenjjenjjen/image/upload/c_scale,h_100,w_100/v1534904521/postgresql-logo_bas0nl.png"
+                  alt="PostgreSQL"
+                />
+
+                <h3>Database</h3>
+              </li>
+
+              <li>
+                <img
+                  src="http://res.cloudinary.com/jjenjjenjjen/image/upload/c_scale,h_100/v1534905883/687474703a2f2f617070732e6f63746f636f6e73756c74696e672e636f6d2f696d616765732f6578707265737349636f6e2e706e67_tygv3e.png"
+                  alt="Express"
+                />
+                <h3>Express</h3>
+              </li>
+              <li>
+                <img
+                  src="http://res.cloudinary.com/jjenjjenjjen/image/upload/c_scale,h_100,w_100/v1534904393/redux-logo-9CA6836C12-seeklogo.com_waoinc.png"
+                  alt="Redux"
+                />
+                <h3>Redux</h3>
+              </li>
+            </ul>
+          </section>
+
+          <section id="second" className="main special">
+            <header className="major">
+              <h2>Projects</h2>
+            </header>
+            <ul className="statistics">
+              <li className="style1">
+                <img src="http://res.cloudinary.com/jjenjjenjjen/image/upload/c_scale,w_500/v1534906162/jenhurdcom_ijdelt.png" />
+                <strong>My Health</strong>
+                Personal Project
+              </li>
+            </ul>
+            <p className="myParagraphs">
+              This was my Personal Project. A healthcare history tracking
+              application with medical provider search feature. Created using:
+              React, Redux, JavaScript, Node, Git/Github, and Material UI. I
+              learned to use Auth0 for user login and Material UI for styling.
+              <br /> View the live site here:{' '}
+              <b>
+                <a href=" http://www.jenhurd.com/">www.jenhurd.com</a>
+              </b>
+            </p>
+
+            <ul className="statistics">
+              <li className="style2">
+                <img src="http://res.cloudinary.com/jjenjjenjjen/image/upload/c_scale,w_500/v1534906171/Freelancer_bonauw.png" />
+                <strong>Freelancer</strong>
+                Group Project
+              </li>
+            </ul>
+            <p className="content">
+              Nam elementum nisl et mi a commodo porttitor. Morbi sit amet nisl
+              eu arcu faucibus hendrerit vel a risus. Nam a orci mi, elementum
+              ac arcu sit amet, fermentum pellentesque et purus. Integer maximus
+              varius lorem, sed convallis diam accumsan sed. Etiam porttitor
+              placerat sapien, sed eleifend a enim pulvinar faucibus semper quis
+              ut arcu. Ut non nisl a mollis est efficitur vestibulum. Integer
+              eget purus nec nulla mattis et accumsan ut magna libero. Morbi
+              auctor iaculis porttitor. Sed ut magna ac risus et hendrerit
+              scelerisque. Praesent eleifend lacus in lectus aliquam porta. Cras
+              eu ornare dui curabitur lacinia.
+            </p>
+            <footer className="major">
+              {/* <ul className="actions">
+                <li>
+                  <Link to="/generic" className="button">
+                    Learn More
+                  </Link>
+                </li>
+              </ul> */}
+            </footer>
+          </section>
+
+          <section id="cta" className="main special">
+            <header className="major">
+              <h2>Congue imperdiet</h2>
+              <p>
+                Donec imperdiet consequat consequat. Suspendisse feugiat congue
+                <br />
+                posuere. Nulla massa urna, fermentum eget quam aliquet.
+              </p>
+            </header>
+            <footer className="major">
+              <ul className="actions">
+                <li>
+                  <Link to="/generic" className="button special">
+                    Get Started
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/generic" className="button">
+                    Learn More
+                  </Link>
+                </li>
+              </ul>
+            </footer>
+          </section>
+        </div>
+      </div>
+    )
+  }
+}
+
+Index.propTypes = {
+  route: React.PropTypes.object,
+}
+
+export default Index
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
